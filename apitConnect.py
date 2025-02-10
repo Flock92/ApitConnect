6fe06cf65d041fcdbda3dd9d54c742e130fc4c9d
Flock92 <113182075+Flock92@users.noreply.github.com>
2025-02-10 23:32:44 +0000

    Update apitConnect.py

--- apitConnect.py
+++ apitConnect.py
@@ -1,5 +1,7 @@
 import json
-from utils.connect import ApitServer
+import asyncio
+from time import sleep
+from bin.connect import ApitServer
 
 
 class Apit212(ApitServer):
@@ -7,20 +9,20 @@ class Apit212(ApitServer):
     def __init__(self, host = "localhost", port = 8080):
         super().__init__(host, port)
 
-    class CFD:
 
-        def __init__(self, mode: str = "demo") -> None:
-            """"""
-            self._mode = mode
+class CFDRequest:
 
-        def get_summary(self) -> json:
-            """Get account summary"""
-            request = {"command":"get_summary", "data":"[]", "mode": self._mode}
-            return json.dumps(request)
-          
-        def get_account(self) -> json:
-            """Get account """
-            request = {"command":"get_account", "data": None, "mode": self._mode}
-            return json.dumps(request)
+    def __init__(self, mode: str = "demo") -> None:
+        """"""
+        self._mode = mode
 
-  
+    def get_summary(self) -> json:
+        """Get account summary"""
+        request = {"command":"get_summary", "data":"[]", "mode": self._mode}
+        return request
+
+    def get_account(self) -> json:
+        """Get account """
+        request = {"command":"get_account", "data": None, "mode": self._mode}
+        return request
+    
